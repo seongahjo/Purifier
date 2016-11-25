225c2f4432ab7ad75aba80f01dbe2dba78e81f93
SuekyeongNam <akasha9890@gmail.com>
2016-11-25 20:40:47 +0900

    add filtering Picture

--- src/main/java/com/skel/util/FilterUtil.java
+++ src/main/java/com/skel/util/FilterUtil.java
@@ -5,6 +5,18 @@ import com.skel.repository.SlangRepository;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.DMatch;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfDMatch;
+import org.opencv.core.MatOfKeyPoint;
+import org.opencv.features2d.DescriptorExtractor;
+import org.opencv.features2d.DescriptorMatcher;
+import org.opencv.features2d.FeatureDetector;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
 import java.util.List;
 
 /**
@@ -36,5 +48,69 @@ public class FilterUtil {
         if(sb.toString().equals(""))
             return content;
         return sb.toString();
+    }
+
+    public static boolean filterPic(String filename1, String filename2){
+        int ret;
+        ret = compareFeature(filename1, filename2);
+        if(ret > 0)
+            return true;
+        else
+            return false;
+    }
+
+    public static int compareFeature(String filename1, String filename2){
+        int retVal = 0;
+
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+        Mat imgO1 = Imgcodecs.imread(filename1, Imgcodecs.CV_LOAD_IMAGE_COLOR);
+        Mat imgO2 = Imgcodecs.imread(filename2, Imgcodecs.CV_LOAD_IMAGE_COLOR);
+
+        Imgproc.cvtColor(imgO1, imgO1, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.cvtColor(imgO2, imgO2, Imgproc.COLOR_BGR2GRAY);
+
+        Mat img1 = new Mat();
+        Mat img2 = new Mat();
+
+        Imgproc.equalizeHist(imgO1, img1);
+        Imgproc.equalizeHist(imgO2, img2);
+
+        //Imgproc.cvtColor(img1, img1, Imgproc.COLOR_GRAY2BGR);
+        //Imgproc.cvtColor(img2, img2, Imgproc.COLOR_GRAY2BGR);
+
+        MatOfKeyPoint keypoints1 = new MatOfKeyPoint();
+        MatOfKeyPoint keypoints2 = new MatOfKeyPoint();
+
+        Mat descriptors1 = new Mat();
+        Mat descriptors2 = new Mat();
+
+        FeatureDetector detector = FeatureDetector.create(FeatureDetector.ORB);
+        DescriptorExtractor extractor = DescriptorExtractor.create(DescriptorExtractor.ORB);
+
+        detector.detect(img1, keypoints1);
+        detector.detect(img2, keypoints2);
+
+        extractor.compute(img1, keypoints1, descriptors1);
+        extractor.compute(img2, keypoints2, descriptors2);
+
+        DescriptorMatcher matcher = DescriptorMatcher.create(DescriptorMatcher.BRUTEFORCE_HAMMING);
+
+        MatOfDMatch matches = new MatOfDMatch();
+
+        if(descriptors2.cols() == descriptors1.cols()) {
+            matcher.match(descriptors1, descriptors2, matches);
+
+            DMatch[] match = matches.toArray();
+            double max_dist = 0;
+            double min_dist = 100;
+
+            for(int i = 0; i < descriptors1.rows(); i++) {
+                if(match[i].distance <= 10){
+                    retVal++;
+                }
+            }
+        }
+        return retVal;
     }
 }
